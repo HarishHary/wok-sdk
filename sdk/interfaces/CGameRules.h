aaf1f5f463659e50b4dc46811541a69224966fef
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-02-24 20:04:58 +0300

    0_o

--- sdk/interfaces/CGameRules.h
+++ sdk/interfaces/CGameRules.h
@@ -2,8 +2,8 @@
 
 class CGameRules {
 public:
-	NETVAR(m_bWarmupPeriod(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data "), _("m_bWarmupPeriod"));
-	NETVAR(m_bFreezePeriod(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data "), _("m_bFreezePeriod"));
-	NETVAR(m_bIsValveDS(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data "), _("m_bIsValveDS"));
+	NETVAR(m_bWarmupPeriod(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data"), _("m_bWarmupPeriod"));
+	NETVAR(m_bFreezePeriod(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data"), _("m_bFreezePeriod"));
+	NETVAR(m_bIsValveDS(), bool, _("DT_CSGameRulesProxy"), _("cs_gamerules_data"), _("m_bIsValveDS"));
 };
 extern CGameRules* g_pGameRules;
