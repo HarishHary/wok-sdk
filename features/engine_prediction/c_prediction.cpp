54ff92c5d4e880992810a39b1c4a0032954dd9bc
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-03-01 14:43:34 +0300

    update engine prediction

--- features/engine_prediction/c_prediction.cpp
+++ features/engine_prediction/c_prediction.cpp
@@ -1,6 +1,26 @@
 #include "c_prediction.h"
 #include "../features.h"
 
+/*
+	not fully reversed 
+	missing some stuff like
+
+  	if ( v19 != -1 && v19 > 0 && v19 <= a2[3339] )
+  	{
+    	a2[63] = -1;
+    	sub_1A3B10(a2, 0);
+    	(*(void (__thiscall **)(_DWORD *))(*a2 + 552))(a2);
+  	}
+
+	if ( *(float *)(g_pGlobalVars + 20) > 0.0 )
+      ++a2[3339];
+	
+	etc....
+
+	i can complete this but.......
+	its up to u
+*/
+
 void c_prediction::run(C_CSPlayer* player, CUserCmd* cmd) {
 	if (!player 
         || g_pEngine->IsPlayingDemo()
@@ -18,16 +38,16 @@ void c_prediction::run(C_CSPlayer* player, CUserCmd* cmd) {
 	backup_curtime = g_pGlobalVars->curtime;
 	backup_frametime = g_pGlobalVars->frametime;
 
-	bool backup_in_prediction = *reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x8);
-	bool backup_is_first_time_predicted = *reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x18);
+	bool backup_in_prediction = *reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x8);
+	bool backup_is_first_time_predicted = *reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x18);
 
-	*reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x8) = true;
-	*reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x18) = false;
+	*reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x8) = true;
+	*reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x18) = false;
 
 	g_pGlobalVars->curtime = TICKS_TO_TIME(player->get_tickbase());
-	g_pGlobalVars->frametime = *reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0xA) ? 0.f : g_pGlobalVars->interval_per_tick;
+	g_pGlobalVars->frametime = *reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0xA) ? 0.f : g_pGlobalVars->interval_per_tick;
 
-	*reinterpret_cast<CUserCmd**>(reinterpret_cast<uint32_t>(player) + 0x3334) = cmd;
+	*reinterpret_cast<CUserCmd**>(uintptr_t(player) + 0x3334) = cmd;
 	*prediction_random_seed = cmd ? MD5_PseudoRandom(cmd->commandnumber) & 0x7FFFFFFF : -1;
 	*prediction_player = reinterpret_cast<int>(player);
 
@@ -44,24 +64,24 @@ void c_prediction::run(C_CSPlayer* player, CUserCmd* cmd) {
 
 	auto vehicle = player->get_vehicle().IsValid() ? reinterpret_cast<C_BaseEntity*>(g_pEntityList->GetClientEntityFromHandle(player->get_vehicle())) : nullptr;
 	if (cmd->impulse && (!vehicle || player->using_standard_weapons_in_vehicle()))
-		*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x31FC) = cmd->impulse;
+		*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x31FC) = cmd->impulse;
 
-	cmd->buttons |= *reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x3330);
+	cmd->buttons |= *reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x3330);
 	int m_nButtons = cmd->buttons;
-	int* m_afButtonLast = reinterpret_cast<int*>(reinterpret_cast<uint32_t>(player) + 0x31F8);
+	int* m_afButtonLast = reinterpret_cast<int*>(uintptr_t(player) + 0x31F8);
 	int buttonsChanged = m_nButtons ^ *m_afButtonLast;
-	*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x31EC) = *m_afButtonLast;
+	*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x31EC) = *m_afButtonLast;
 	*m_afButtonLast = m_nButtons;
-	*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x31F0) = m_nButtons & buttonsChanged;
-	*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x31F4) = buttonsChanged & ~m_nButtons;
+	*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x31F0) = m_nButtons & buttonsChanged;
+	*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x31F4) = buttonsChanged & ~m_nButtons;
 
 	g_pPrediction->CheckMovingOnGround(player, g_pGlobalVars->frametime);
+
 	player->set_local_view_angles(cmd->viewangles);
 
-	if (player->physics_run_think(0))
-		player->pre_think();
+	player->physics_run_think(0) ? player->pre_think() : 0;
 
-	auto next_think = reinterpret_cast<int*>(reinterpret_cast<uint32_t>(player) + 0xFC);
+	auto next_think = reinterpret_cast<int*>(uintptr_t(player) + 0xFC);
 	if (*next_think > 0 && *next_think <= player->get_tickbase()) {
 		*next_think = -1;
 		player->think();
@@ -71,18 +91,17 @@ void c_prediction::run(C_CSPlayer* player, CUserCmd* cmd) {
 	g_pPrediction->SetupMove(player, cmd, g_pMoveHelper, &movedata);
 
 	if (vehicle)
-		utils::get_vfunc<void(__thiscall*)(int, C_BaseEntity*, void*)>(vehicle, 5)(reinterpret_cast<uint32_t>(vehicle), player, &movedata);
+		utils::get_vfunc<void(__thiscall*)(int, C_CSPlayer*, void*)>(vehicle, 5)(uintptr_t(vehicle), player, &movedata);
 	else
 		g_pMovement->ProcessMovement(player, &movedata);
 
 	g_pPrediction->FinishMove(player, cmd, &movedata);
-
 	g_pMoveHelper->ProcessImpacts();
 
 	post_think(player);
 
-	*reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x8) = backup_in_prediction;
-	*reinterpret_cast<bool*>(reinterpret_cast<uint32_t>(g_pPrediction) + 0x18) = backup_is_first_time_predicted;
+	*reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x8) = backup_in_prediction;
+	*reinterpret_cast<bool*>(uintptr_t(g_pPrediction) + 0x18) = backup_is_first_time_predicted;
 }
 
 void c_prediction::end(C_CSPlayer* player, CUserCmd* cmd) {
@@ -93,12 +112,11 @@ void c_prediction::end(C_CSPlayer* player, CUserCmd* cmd) {
 
 	g_pMovement->FinishTrackPredictionErrors(player);
 	g_pMoveHelper->SetHost(nullptr);
+	g_pMovement->Reset();
 
-	*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x3334) = 0;
+	*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0x3334) = 0;
 	*prediction_random_seed = 0;
 	*prediction_player = 0;
-
-	g_pMovement->Reset();
 
 	g_pGlobalVars->curtime = backup_curtime;
 	g_pGlobalVars->frametime = backup_frametime;;
--- features/engine_prediction/c_prediction.h
+++ features/engine_prediction/c_prediction.h
@@ -10,24 +10,24 @@ private:
 	int* prediction_player;
 	int* prediction_random_seed;
 
-    int post_think(C_CSPlayer* player){
+    int post_think(C_CSPlayer* player) { // 56 8B 35 ? ? ? ? 57 8B F9 8B CE 8B 06 FF 90 ? ? ? ? 8B 07 (client_panorama.dll)
 		utils::get_vfunc<void(__thiscall*)(void*)>(g_pModelCache, 33)(g_pModelCache);
 		if (player->is_alive()) {
 			utils::get_vfunc<void(__thiscall*)(void*)>(player, 339)(player);
 
 			if (player->get_flags() & FL_ONGROUND) 
-				*reinterpret_cast<uintptr_t*>(uintptr_t(player) + 0x3014) = 0;
+				*reinterpret_cast<uintptr_t*>(uintptr_t(player) + 0xC05) = 0;
 
-			if (*reinterpret_cast<uint32_t*>(reinterpret_cast<uint32_t>(player) + 0x28BC) == -1)
+			if (*reinterpret_cast<uint32_t*>(uintptr_t(player) + 0xA2F) == -1)
 				utils::get_vfunc<void(__thiscall*)(void*, int)>(player, 218)(player, 0);
 
 			utils::get_vfunc<void(__thiscall*)(void*)>(player, 219)(player);
 
-			static const auto post_think_v_physics = reinterpret_cast<bool(__thiscall*)(C_BaseEntity*)>(SIG("client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB"));
+			static const auto post_think_v_physics = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB"));
 			post_think_v_physics(player);
 		}
 
-		static const auto simulate_player_simulated_entities = reinterpret_cast<bool(__thiscall*)(C_BaseEntity*)>(SIG("client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72"));
+		static const auto simulate_player_simulated_entities = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72"));
 		simulate_player_simulated_entities(player);
 
 		return utils::get_vfunc<int(__thiscall*)(void*)>(g_pModelCache, 34)(g_pModelCache);
