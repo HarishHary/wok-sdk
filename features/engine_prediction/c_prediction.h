0e282202688c16e29875cb32127ab9b33133b56a
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-03-01 14:45:47 +0300

    Update c_prediction.h

--- features/engine_prediction/c_prediction.h
+++ features/engine_prediction/c_prediction.h
@@ -23,12 +23,12 @@ private:
 
 			utils::get_vfunc<void(__thiscall*)(void*)>(player, 219)(player);
 
-			static const auto post_think_v_physics = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB"));
-			post_think_v_physics(player);
+			static const auto post_think_v_physics_fn = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB"));
+			post_think_v_physics_fn(player);
 		}
 
-		static const auto simulate_player_simulated_entities = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72"));
-		simulate_player_simulated_entities(player);
+		static const auto simulate_player_simulated_entities_fn = reinterpret_cast<bool(__thiscall*)(C_CSPlayer*)>(SIG("client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72"));
+		simulate_player_simulated_entities_fn(player);
 
 		return utils::get_vfunc<int(__thiscall*)(void*)>(g_pModelCache, 34)(g_pModelCache);
 	}
